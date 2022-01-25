d912b2967905d83dfb1cc6493a2e333668046a16
Francisco <contact.frann@gmail.com>
2022-01-25 15:20:08 -0300

    Login Panel for SuitedHosting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bulma/css/bulma.min.css";
+import { Form, Button, Icon, Box } from "react-bulma-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignInAlt, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Box
+          backgroundColor="dark"
+          textAlign="left"
+          textColor="white"
+          style={{ width: 450, height: 340 }}
         >
-          Learn React
-        </a>
+          <Form.Field>
+            <label className="App-label">Email</label>
+            <Form.Control>
+              <Form.Input
+                placeholder="contact.frann@gmail.com"
+                name="email"
+                type="email"
+              />
+              <Icon align="left">
+                <FontAwesomeIcon icon={faSignInAlt} color="black" />
+              </Icon>
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <label className="App-label">Password</label>
+            <Form.Control>
+              <Form.Input
+                placeholder="*********"
+                name="password"
+                type="password"
+              />
+              <Icon align="left">
+                <FontAwesomeIcon icon={faUnlockAlt} color="black" />
+              </Icon>
+            </Form.Control>
+          </Form.Field>
+          <div className="rememberMe">
+            <label className="App-label">
+              <input className="App-input" name="isGoing" type="checkbox" />
+              Remember me
+            </label>
+          </div>
+          <Button.Group>
+            <Button fullwidth color="link">
+              Login
+            </Button>
+            <a className="forgotPassword">Forgot your password?</a>
+          </Button.Group>
+        </Box>
       </header>
     </div>
   );
